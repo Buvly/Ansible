05da94254043e401b40fbc982c45a0ba70e8da04
Buvly <Buvly.w@gmail.com>
2023-11-05 17:26:04 +0200

    Committing

--- push.sh
+++ push.sh
@@ -6,4 +6,4 @@ token=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
 
 git add .
 git commit -m "Committing"
-git push https://$token@github.com/$username/Ansible.git
+git push https://$(token)@github.com/$(username)/Ansible.git
